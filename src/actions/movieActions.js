a75aacc796fca9fed75b0e37a6a6905de04c3cad
Gulbeyaz <glbzbayram@gmail.com>
2023-09-26 14:10:52 +0300

    ADD_MOVİE action'ı eklendi

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,5 +1,10 @@
 export const DELETE_MOVIE = "DELETE_MOVIE";
+export const ADD_MOVIE = "Yeni film datasını ekler";
 
 export const deleteMovie = (id) => {
-  return ({ type: DELETE_MOVIE, payload: id });
-}
+  return { type: DELETE_MOVIE, payload: id };
+};
+
+export const addMovie = (new_movie) => {
+  return { type: ADD_MOVIE, payload: new_movie };
+};
