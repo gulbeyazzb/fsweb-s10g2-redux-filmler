cf67bd38984994c87a27152ff7fdb0c6b44574fd
Gulbeyaz <glbzbayram@gmail.com>
2023-09-26 23:22:25 +0300

    favoriteMovies reducer combineReducer'da tanımlandı

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import movieReducer from "./movieReducer";
+import favouriteMovieReducer from "./favouriteMovies";
 
 const reducers = combineReducers({
   movie: movieReducer,
+  favoriteMovies: favouriteMovieReducer,
 });
 export default reducers;
